4d3eb3cf623b3aca65753e7b929c30c5fd2b5cdd
clucasl3-hue <clucasl3@unemi.edu.ec>
2025-09-21 22:02:02 -0500

    Update Bloque 1 Ejercicio 3.js

--- Bloque 1 Ejercicio 3.js
+++ Bloque 1 Ejercicio 3.js
@@ -1,6 +1,6 @@
 let edades = [];
-for (let i = 0; i < 3; i++) {
-    edades[i] = parseInt(prompt("Ingrese la edad del hermano " + (i + 1) + ":"));
+for (let i = 1; i <= 3; i++) {
+    edades.push(parseInt(prompt(`Ingrese la edad del hermano ${i}`)));
 }
 let mayor = edades[0];
 for (let i = 1; i < edades.length; i++) {
@@ -8,4 +8,4 @@ for (let i = 1; i < edades.length; i++) {
         mayor = edades[i];
     }
 }
-console.log("El hermano mayor tiene " + mayor + " años.");
+console.log(`El hermano mayor tiene ${mayor} años`);
