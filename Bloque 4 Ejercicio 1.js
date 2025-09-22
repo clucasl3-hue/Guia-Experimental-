6edc6c599a0b6d10d93f5b23a589a3b29a57b803
clucasl3-hue <clucasl3@unemi.edu.ec>
2025-09-21 22:44:33 -0500

    Create Bloque 4 Ejercicio 1.js

---
+++ Bloque 4 Ejercicio 1.js
@@ -0,0 +1,17 @@
+let usuarios, costoT=0, costoU=0;
+usuarios=parseInt(prompt("Ingrese la cantidad de usuarios:"));
+if (usuarios >= 100){
+  costoT=usuarios*5;
+  costoU= 5;
+}else if (usuarios >= 50) {
+  costoT=usuarios*6;
+  costoU= 6;
+}else if (usuarios >= 30) {
+  costoT=usuarios*8;
+  costoU= 8;
+}else {
+  costoT=300;
+  costoU=costoT/usuarios;
+}
+console.log (`Costo total: $${costoT}`);
+console.log (`Costo por usuario: $${costoU}`);
